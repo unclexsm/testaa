67f581627c917ca530e263050915317740f3fad9
unclexsm <2820843854@qq.com>
2019-01-21 10:49:48 +0800

    test

---
+++ src/main/java/ui/xsm/com/mylibrary/Test.java
@@ -0,0 +1,22 @@
+package ui.xsm.com.mylibrary;
+
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * @Created by XSM大叔
+ * @CreateTime on 2019/1/20 12:32
+ * @viesion：1.0.0
+ * @Copyright：
+ * @Website：
+ * @E-Mail Address：2820843854@qq.com
+ * @Discription 功能描述：
+ */
+
+
+public class Test {
+
+    public static void test() {
+        Log.e("tes ","test——————————————");
+    }
+}
